b397eb2efa7dab18b5eb5981fcc4cb23a9668392
ghoti57 <35867995+ghoti57@users.noreply.github.com>
2020-02-17 17:59:56 +0000

    Digital Debug
    Updated methods for accessing digital debug pins

--- config.h
+++ config.h
@@ -13,22 +13,36 @@
 #  error "No hardware target defined"
 #endif
 
-#define USE_FIFO
-
 #if defined(DEBUG_PORT)
-#define DEBUG1_ON    ( DEBUG_PORT |=  DEBUG_PIN1 );
-#define DEBUG1_OFF   ( DEBUG_PORT &= ~DEBUG_PIN1 );
-#define DEBUG2_ON    ( DEBUG_PORT |=  DEBUG_PIN2 );
-#define DEBUG2_OFF   ( DEBUG_PORT &= ~DEBUG_PIN2 );
-#define DEBUG3_ON    ( DEBUG_PORT |=  DEBUG_PIN3 );
-#define DEBUG3_OFF   ( DEBUG_PORT &= ~DEBUG_PIN3 );
+
+#define DEBUG_MASK   ( DEBUG_PIN1 | DEBUG_PIN2 | DEBUG_PIN3 | DEBUG_PIN4 )
+
+#define DEBUG1_ON    do{ DEBUG_PORT |=  DEBUG_PIN1; } while(0)
+#define DEBUG1_OFF   do{ DEBUG_PORT &= ~DEBUG_PIN1; } while(0)
+#define DEBUG2_ON    do{ DEBUG_PORT |=  DEBUG_PIN2; } while(0)
+#define DEBUG2_OFF   do{ DEBUG_PORT &= ~DEBUG_PIN2; } while(0)
+#define DEBUG3_ON    do{ DEBUG_PORT |=  DEBUG_PIN3; } while(0)
+#define DEBUG3_OFF   do{ DEBUG_PORT &= ~DEBUG_PIN3; } while(0)
+#define DEBUG4_ON    do{ DEBUG_PORT |=  DEBUG_PIN4; } while(0)
+#define DEBUG4_OFF   do{ DEBUG_PORT &= ~DEBUG_PIN4; } while(0)
+
 #else
-#define DEBUG1_ON
-#define DEBUG1_OFF
-#define DEBUG2_ON
-#define DEBUG2_OFF
-#define DEBUG3_ON
-#define DEBUG3_OFF
+
+#define DEBUG1_ON   do{}while(0)
+#define DEBUG1_OFF  do{}while(0)
+#define DEBUG2_ON   do{}while(0)
+#define DEBUG2_OFF  do{}while(0)
+#define DEBUG3_ON   do{}while(0)
+#define DEBUG3_OFF  do{}while(0)
+#define DEBUG4_ON   do{}while(0)
+#define DEBUG4_OFF  do{}while(0)
+#define DEBUG_OUT   do{}while(0)
+
 #endif
+
+#define DEBUG1(_v) do{ if(_v) DEBUG1_ON; else DEBUG1_OFF; }while(0)
+#define DEBUG2(_v) do{ if(_v) DEBUG2_ON; else DEBUG2_OFF; }while(0)
+#define DEBUG3(_v) do{ if(_v) DEBUG3_ON; else DEBUG3_OFF; }while(0)
+#define DEBUG4(_v) do{ if(_v) DEBUG4_ON; else DEBUG4_OFF; }while(0)
 
 #endif
