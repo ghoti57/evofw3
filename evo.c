1cbb93f517724d04e0f4e990a6681589a3f3b905
ghoti57 <35867995+ghoti57@users.noreply.github.com>
2020-02-17 18:00:33 +0000

    Main system changes
    Use the new functionality

--- evo.c
+++ evo.c
@@ -1,36 +1,41 @@
 #include <avr/interrupt.h>
 #include <avr/wdt.h>
+
 #include "config.h"
-#include "bitstream.h"
 #include "tty.h"
-#include "cc1101.h"
 #include "led.h"
-#include "transcoder.h"
+
+#include "spi.h"
+#include "cc1101.h"
+#include "ringbuf.h"
+
+#include "frame.h"
+#include "message.h"
 
 void main_init(void) {
   // OSCCAL=((uint32_t)OSCCAL * 10368) / 10000;
 
-  wdt_disable();
-  led_init();
-
 #if defined(DEBUG_PORT)
-  DEBUG_DDR |= ( DEBUG_PIN1 + DEBUG_PIN2 + DEBUG_PIN3 );
+  DEBUG_DDR  = DEBUG_MASK;
+  DEBUG_PORT = 0;
 #endif
 
-  // Wire up components
-  transcoder_init(&tty_write_str, 0);//&driver_send_byte);
-  bs_init();
-  tty_init(&transcoder_accept_outbound_byte);
-  cc_init(0,0);//&driver_accept_bit, &driver_request_bit);
+  wdt_disable();
+  led_init();
+  tty_init(0);
 
-  led_off();
+  // Wire up components
+  spi_init();
+  cc_init();
+  frame_init();
+  msg_init();
+  
   sei();
 }
 
 void main_work(void) {
-  transcoder_work();
-  tty_work();
-  cc_work();
+  frame_work();
+  msg_work();
 }
 
 #ifdef NEEDS_MAIN
