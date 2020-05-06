356615b4bef789a0a6d9a5807867ee060259843d
ghoti57 <ghoti57@gmail.com>
2020-05-06 16:32:16 +0100

    Set version to evofw3 0.3.0

--- version.h
+++ version.h
@@ -5,10 +5,10 @@
 #ifndef _VERSION_H_
 #define _VERSION_H_
 
-#define BRANCH "evofw3\\uart"
+#define BRANCH "evofw3"
 
 #define MAJOR  0
 #define MINOR  3
-#define SUBVER 2
+#define SUBVER 0
 
 #endif // _VERSION_H_
