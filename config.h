e2609769cdfcf78821348c4b8a1755110065eacf
ghoti57 <ghoti57@gmail.com>
2021-02-28 14:11:33 +0000

    Allow arduino gui to configure host baudrate Force build with evofw3_avr board definitions

--- config.h
+++ config.h
@@ -3,6 +3,8 @@
 
 #if defined ARDUINO_EVOFW3_ATMEGA32U4
 #include "atm32u4_pins.h"
+#define HOST_RATE 0
+
 #elif defined ARDUINO_EVOFW3_ATMEGA328P
 #include "atm328_pins.h"
 
@@ -29,6 +31,11 @@
 #include "debug.h"
 
 #define SPI_CLK_RATE    250000
-#define TTY_BAUD_RATE   115200
+
+#if defined(HOST_RATE)
+#define TTY_BAUD_RATE   HOST_RATE
+#else
+#error No Host baud rate defined - use evofw3_avr board definitions
+#endif
 
 #endif
